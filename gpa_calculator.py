b200e3c1af45bff5449bbf8be4dae7b567b1ae6f
fmr2689 <fmr2689@rit.edu>
2022-04-05 14:53:09 -0400

    thoughts comment

---
+++ gpa_calculator.py
@@ -0,0 +1,18 @@
+'''
+Thoughts: 
+
+Make a class for Student 
+    would allow for calculating overall GPA 
+
+Make a class for course
+    allow for getting gpa quality points for course 
+
+Make a func/class for grades 
+    input number of assignments, weight, grade, returns 
+    grade for the class, possibly with reference from 
+    syllabus to what grade correspond to letter grade
+
+Make globals for past semesters? 
+    so that can just be passed in? maybe? 
+    or store in some other way
+'''
