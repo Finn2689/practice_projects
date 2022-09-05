912df207b66e7729ff3a8a8781b4cc0677bcafef
fmr2689 <fmr2689@rit.edu>
2022-09-05 12:39:14 -0400

    put some data in

---
+++ crime_calculator.py
@@ -0,0 +1,12 @@
+
+new_file = open("filename") 
+# parse through the first line, create categories for each of the csv values 
+# go through and store 
+# types of analysis: mean, median, rate per 100,000
+# only print out the relevant stats 
+# make a chart 
+# percent change if it's from year to year? 
+
+
+
+new_file.close()
