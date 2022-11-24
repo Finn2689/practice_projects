cad8a921cfc65635abfa655fdf51b68b852b73a4
fmr2689 <fmr2689@rit.edu>
2022-11-24 00:31:15 -0500

    created deck class and method to shuffle cards

---
+++ theGame/Deck.java
@@ -0,0 +1,42 @@
+package theGame;
+
+import java.util.Random;
+
+public class Deck { 
+    private final int cardCount = 99; 
+    private final int[] unshuffledDeck = new int[99]; 
+    private int[] shuffledDeck; 
+
+    public Deck() { 
+        for(int i=1; i<cardCount; i++) { 
+            unshuffledDeck[i] = i; 
+        }
+        this.shuffledDeck = shuffle(this.unshuffledDeck); 
+    }
+
+    private int[] shuffle(int[] unshuffledDeck) { 
+        Random rand = new Random(); 
+        for(int n=0; n<99; n++) { 
+            int indexToSwap = rand.nextInt(unshuffledDeck.length); 
+            int temp = unshuffledDeck[indexToSwap]; 
+            unshuffledDeck[indexToSwap] = unshuffledDeck[n]; 
+            unshuffledDeck[n] = temp; 
+        }
+        this.shuffledDeck = unshuffledDeck; 
+        return this.shuffledDeck; 
+    }
+
+    @Override 
+    public String toString() { 
+        String result = ""; 
+        for(int card : this.shuffledDeck) { 
+            result += "[" + card + "]\t "; 
+        }
+        return result; 
+    }
+
+    public static void main(String[] args) { 
+        Deck deck = new Deck(); 
+        System.out.println(deck); 
+    }
+}
