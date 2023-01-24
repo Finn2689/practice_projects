1f867210453056074e88995d103f154fb09a1072
fmr2689 <fmr2689@rit.edu>
2023-01-24 13:37:47 -0500

    starting setup for everyCombo

---
+++ randomQuestions/everyCombo.py
@@ -0,0 +1,11 @@
+
+def main(): 
+    # print(1)
+    userRange = input("Enter the highest digit you want to see: ")
+    userRange = int(userRange)
+    userDigits = input("Enter how many places you want to see: ")
+    userDigits = int(userDigits)
+
+    # print(userRange)
+
+main()
